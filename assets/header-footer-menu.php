34b7023a053dd69868fff9101ba8625ae53d278c
Carlos <1971975680@undc.edu.pe>
2023-09-23 16:17:00 -0500

    Initial commit

---
+++ assets/header-footer-menu.php
@@ -0,0 +1,12 @@
+<div class="header header-fixed header-logo-center header-auto-show">
+        <a href="#header-title" class="header-title back-to-top">LunApp</a>
+        <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
+        <a href="#" data-toggle-theme class="header-icon header-icon-4"><i class="fas fa-lightbulb"></i></a>
+    </div>
+
+    <div id="footer-bar" class="footer-bar-1">
+    
+        <a href="destacados.php"><i class="fa fa-star"></i><span>Destacados</span></a>
+		<a href="menu.php" class="active-nav"><i class="fa fa-home"></i><span>Inicio</span></a>
+        <a href="#" data-menu="menu-settings"><i class="fa fa-cog"></i><span>Ajustes</span></a>
+    </div>
