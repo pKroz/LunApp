d2eb3355cd36fdcdc4aa34f9a961a437fb24ae60
Carlos <1971975680@undc.edu.pe>
2023-10-02 13:17:53 -0500

    wa

--- menu.php
+++ menu.php
@@ -68,7 +68,7 @@
 		<div class="content mt-2 mb-n3">
 			<h5 class="mb-2">Lo más visto</h5>
 			<div class="row">
-				<div class="col-6 pe-2">					<?php
+				<div class="col-6 ps-2">					<?php
 					include('./config.php');
 					$sqlActividades1 = ("SELECT * FROM `actividades`");
 					$queryActividades1 = mysqli_query($con, $sqlActividades1);
