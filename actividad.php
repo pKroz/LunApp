6db8271a5e02c8353959faf2bdfda4c2dff42b98
Carlos <1971975680@undc.edu.pe>
2023-10-06 10:09:19 -0500

    wa

--- actividad.php
+++ actividad.php
@@ -63,7 +63,7 @@
 
                         <div class="d-flex pb-0">
                             <div class="align-self-center flex-grow-1">
-                                <strong class="color-theme font-20 d-block mt-0 mb-0 pb-0">Canopy</strong>
+                                <strong class="color-theme font-20 d-block mt-0 mb-0 pb-0"><?php echo $dataActividad['actividad']; ?></strong>
                                 <span class="font-11 color-theme opacity-30 d-block pb-2"><i
                                         class="fa fa-clock pe-1"></i><?php echo $dataActividad['horaInicio']; ?> -
                                     <?php echo $dataActividad['horaFin']; ?></span>
