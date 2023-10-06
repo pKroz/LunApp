e777a1460967f042d0d6f53fba9476679aee348c
Carlos <1971975680@undc.edu.pe>
2023-10-06 09:52:36 -0500

    WA

--- info.php
+++ info.php
@@ -5,7 +5,7 @@
 <body class="theme-light" data-highlight="highlight-red" data-gradient="body-default">
 <div id="preloader"><div class="spinner-border color-highlight" role="status"></div></div>
 <div id="page">
-    <?php include_once './assets/header-footer-act.php'?>
+    <?php include_once './assets/header-footer-info.php'?>
     
     <div class="page-content header-clear-medium">
 
@@ -14,25 +14,26 @@
 			
             <div class="content mt-n5 pt-3">
                 <div>
-                    <span class="color-highlight font-300 d-block text-uppercase font-10 pt-3">Actividades en Lunahuana</span>
-                  
                     <div class="d-flex pb-0">
                     <div class="align-self-center flex-grow-1">
-                        <strong class="color-theme font-20 d-block mt-0 mb-0 pb-0">Canopy</strong>
-                        <span class="font-11 color-theme opacity-30 d-block pb-2"><i class="fa fa-clock pe-1"></i>s</span>
+                        <strong class="color-theme font-20 d-block mt-0 mb-0 pb-0"></strong>
+                        <span class="font-11 color-theme opacity-30 d-block pb-2"><i class="fa fa-clock pe-1"></i>EMPRESA</span>
 
                     </div>
                     <div class="align-self-center flex-shrink-1">
                         <a href="#" class="btn btn-full btn-sm font-900 text-uppercase rounded-sm shadow-xxl bg-highlight mb-3"><i class="fa fa-vr-cardboard pe-2 me-1 font-15 color-white"></i>Realizar tour</a>
                     </div>
+                    </div>
                 </div>
-                </div>
-
-                <h4>¿En qué consiste la actividad?</h4>
+                <div class="divider mt-4"></div>
+                <h5>INFORMACIÓN 1</h5>
                 <p class="mb-4">
-                Deporte extremo que consiste en el desplazamiento de un punto a otro mediante cables horizontales que están trazados en bosques entre las ramas de árboles. Y así sentir y vivir la sensación de volar, como también apreciar el paisaje y la naturaleza desde las alturas
+                Información de la empresa 1.
                 </p>
-
+                <h5>INFORMACIÓN 2</h5>
+                <p class="mb-4">
+                Información de la empresa 2.
+                </p>
 
                 <div class="card rounded-sm overflow-hidden" data-card-height="400">
                 <div class="divider mt-4"></div>
