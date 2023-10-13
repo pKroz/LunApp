2e93d64ad4fe74f762b89a768fbe08609adbb529
Carlos <1971975680@undc.edu.pe>
2023-10-12 22:51:42 -0500

    wa

--- menu.php
+++ menu.php
@@ -38,7 +38,7 @@
 							<h6 class="font-12 opacity-60 mb-2">ACTIVIDAD</h6>
 						</div>
                         <div class="pe-2 align-self-center ms-auto">
-							<h6 class="font-12 opacity-60 mb-2">HORARIO</h6>
+							<h6 class="font-12 opacity-60 mb-2">DURACIÓN - PRECIO</h6>
 						</div>
 					<a>
 					<a data-filter-name="all" class="divider mb-1 mx-2"></a>
@@ -54,7 +54,7 @@
                             <strong class="color-theme font-16 d-block"><?php echo ucfirst($dataActividad['actividad']); ?></strong>
                         </div>
                         <div class="ms-auto text-end align-self-center pe-2">
-                            <h5 class="line-height-xs font-14 pt-1 opacity-40"><?php echo $dataActividad['tiempo']; ?></h5>
+                            <h5 class="line-height-xs font-14 pt-1 opacity-40"><?php echo $dataActividad['tiempo']; ?> - S/. <?php echo $dataActividad['precio']; ?></h5>
                             
                         </div>
                     </a>
