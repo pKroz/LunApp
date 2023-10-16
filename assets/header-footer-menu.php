ebbce853d8a093775d7c3ccd859c8b86a640bdb5
Carlos <1971975680@undc.edu.pe>
2023-10-15 21:35:19 -0500

    wa

--- assets/header-footer-menu.php
+++ assets/header-footer-menu.php
@@ -1,5 +1,5 @@
 <div class="header header-fixed header-logo-center header-auto-show">
-        <a href="menu.php" class="header-title back-to-top">LunApp</a>
+        <a href="menu.php" class="header-title back-to-top">Travel & Tour</a>
         <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-4"><i class="fas fa-lightbulb"></i></a>
     </div>
