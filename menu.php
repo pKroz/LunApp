88fe40b0a6aa7e06c9c7692cc1ec46651e4d0662
Carlos <1971975680@undc.edu.pe>
2023-10-16 21:26:59 -0500

    wa

--- menu.php
+++ menu.php
@@ -14,105 +14,266 @@
 
     <div class="page-content">
 
-		<div class="card card-style mx-0 mb-0 rounded-0" data-card-height="400" style="background-image:url('images/pictures_vertical/bg1.jpg')">
-			<div class="card-top">
-				
-			</div>
-			<div class="card-bottom mb-4">
-				<h1 class="color-white text-center mb-0 font-28">Atractivos de Lunahuana</h1>
-				<p class="color-white text-center mb-3 ">Encuentra los sitios más visitados aquí!</p>
-			</div>
-			<div class="card-overlay bg-gradient rounded-0"></div>
-		</div>
-
-        <div class="content mt-n4">
-            <div class="search-box bg-theme color-theme rounded-m shadow-l">
-                <i class="fa fa-search"></i>
-                <input type="text" class="border-0" placeholder="Buscar una actividad" data-search id="search-input">
-				<a href="#" class="clear-search disabled no-click mt-0"></a>
-            </div>
-            <div class="search-results disabled-search-list mt-3">
-                <div class="card card-style mx-0 px-2 py-2 mb-0">
-					<a href="#" class="d-flex py-1" data-filter-name="all">
-                        <div class="ps-2 align-self-center">
-							<h6 class="font-12 opacity-60 mb-2">ACTIVIDAD</h6>
-						</div>
-						<div class="pe-2 align-self-center ms-auto">
-							<h6 class="font-12 opacity-60 mb-2 text-end">PRECIO</h6>
-							<h6 class="font-12 opacity-60 mb-2 text-end">DURACIÓN</h6>
-						</div>
-					<a>
-					<a data-filter-name="all" class="divider mb-1 mx-2"></a>
-					<?php
-					include('./config.php');
-					$sqlActividades = ("SELECT * FROM `actividades`");
-					$queryActividades = mysqli_query($con, $sqlActividades);
-					?>
-					<?php 
-                    while ($dataActividad = mysqli_fetch_array($queryActividades)) { ?>
-                    <a href="actividad.php?id=<?php echo $dataActividad['id']; ?>" class="d-flex" data-filter-item data-filter-name="all <?php echo strtolower($dataActividad['actividad']); ?>">
-                        <div class="ps-2 align-self-center">
-                            <strong class="color-theme font-16 d-block"><?php echo strtolower($dataActividad['actividad']); ?></strong>
-                        </div>
-                        <div class="ms-auto text-end align-self-center pe-2">
-							<h5 class="line-height-xs font-14 pt-1 mb-1 opacity-40">S/. <?php echo $dataActividad['precio']; ?></h5>
-                            <h5 class="line-height-xs font-14 opacity-30"><?php echo trim($dataActividad['tiempo'],"."); ?></h5>
-						</div>
-                    </a>
-					<?php } 
-                    ?>
-                    
+	<div class="content notch-clear">
+            <div class="d-flex pt-2">
+                <div class="align-self-center me-auto">
+                    <strong class="text-uppercase opacity-60 font-12">Welcome</strong>
+                    <h1 class="mt-n2 font-30">Explore</h1>
                 </div>
-            </div>	
+                <div class="align-self-center ms-auto mt-1">
+                    <a href="#" class="d-block"><img src="images/pictures/6s.jpg" class="img-fluid shadow-xl rounded-circle" width="52" alt="img"></a>
+                </div>
+            </div>
         </div>
 
-		<!-- <div class="content mt-2 mb-n3">
-			<h5 class="mb-2">Lo más visto</h5>
-			<div class="row">
-				<div class="col-6 ps-2">
-					
-					<a href="" class="card card-style mx-0 mb-3">
-						<div class="card-top m-2">
-							<span class="bg-highlight color-black font-11 px-2 py-1 font-700 rounded-xs shadow-xxl">15% OFF</span>
-						</div>
-						<img src="images/media/img4.jpg" alt="img" class="img-fluid">
-						<div class="p-2 pb-0">
-							<h4 class="mb-1"></h4>
-							<p class="pb-1 mb-0 font-11 mt-n1 opacity-70"></i>a</p>
-						</div>
-						
+        <div class="content mt-n1">
+            <div class="search-box bg-theme color-theme rounded-m shadow-l">
+                <i class="fa fa-search"></i>
+                <input type="text" class="border-0" placeholder="Search for a place... (try island)" data-search>
+				<a href="#" class="clear-search disabled mt-0"><i class="fa fa-times color-red-dark"></i></a>
+            </div>
+            <div class="search-results disabled-search-list mt-3">
+                <div class="card card-style mx-0 px-2 p-0 mb-0">
+                    <a href="#" class="d-flex py-2" data-filter-item data-filter-name="all maldives tropical island escape">
+                        <div>
+                            <img src="images/travel/6s.jpg" class="rounded-sm me-3" width="50" alt="img">
+                        </div>
+                        <div>
+                            <span class="color-highlight font-400 d-block pt-0 text-uppercase font-10">Tropical Island</span>
+                            <strong class="color-theme font-16 d-block mt-n2">Maldives</strong>
+                        </div>
+                        <div class="ms-auto text-end align-self-center pe-2">
+                            <h5 class="line-height-xs font-18 pt-3">$1450</h5>
+                            <span class="font-10 d-block mt-n2">For 7 Nights</span>
+                        </div>
+                    </a>
+                    <a href="#" class="d-flex py-2" data-filter-item data-filter-name="all weekend iceland escape explore hike">
+                        <div>
+                            <img src="images/travel/7s.jpg" class="rounded-sm me-3" width="50" alt="img">
+                        </div>
+                        <div>
+                            <span class="color-highlight font-400 d-block pt-0 text-uppercase font-10">Weekend Escape</span>
+                            <strong class="color-theme font-16 d-block mt-n2">Iceland</strong>
+                        </div>
+                        <div class="ms-auto text-end align-self-center pe-2">
+                            <h5 class="line-height-xs font-18 pt-3">$675</h5>
+                            <span class="font-9 d-block mt-n2">For 3 Days</span>
+                        </div>
+                    </a>
+                    <a href="#" class="d-flex py-2" data-filter-item data-filter-name="all cave swim explore deep dive">
+                        <div>
+                            <img src="images/travel/5s.jpg" class="rounded-sm me-3" width="50" alt="img">
+                        </div>
+                        <div>
+                            <span class="color-highlight font-400 d-block pt-0 text-uppercase font-10">Cave Exploring</span>
+                            <strong class="color-theme font-16 d-block mt-n2">Nicaragua</strong>
+                        </div>
+                        <div class="ms-auto text-end align-self-center pe-2">
+                            <h5 class="line-height-xs font-18 pt-3">$350</h5>
+                            <span class="font-9 d-block mt-n2">For 3 Dives</span>
+                        </div>
+                    </a>
+                    <a href="#" class="d-flex py-2" data-filter-item data-filter-name="all mountain cabin tent forest">
+                        <div>
+                            <img src="images/travel/1s.jpg" class="rounded-sm me-3" width="50" alt="img">
+                        </div>
+                        <div>
+                            <span class="color-highlight font-400 d-block pt-0 text-uppercase font-10">Private Escape</span>
+                            <strong class="color-theme font-16 d-block mt-n2">Mount Hellen</strong>
+                        </div>
+                        <div class="ms-auto text-end align-self-center pe-2">
+                            <h5 class="line-height-xs font-18 pt-3">$1250</h5>
+                            <span class="font-9 d-block mt-n2">7 Days + Cabin</span>
+                        </div>
+                    </a>
+                    <a href="#" class="d-flex py-2" data-filter-item data-filter-name="all ocean cruise ship yacht swim">
+                        <div>
+                            <img src="images/travel/4s.jpg" class="rounded-sm me-3" width="50" alt="img">
+                        </div>
+                        <div>
+                            <span class="color-highlight font-400 d-block pt-0 text-uppercase font-10">Ocean Cruise</span>
+                            <strong class="color-theme font-16 d-block mt-n2">Dubrovnik</strong>
+                        </div>
+                        <div class="ms-auto text-end align-self-center pe-2">
+                            <h5 class="line-height-xs font-18 pt-3">$2350</h5>
+                            <span class="font-9 d-block mt-n2">7 Days + Cruise</span>
+                        </div>
+                    </a>
 
-					</a>
-					
-				</div>
+                </div>
+            </div>
+        </div>
+        <div class="search-no-results disabled mt-4">
+            <div class="card card-style">
+                <div class="content">
+                    <h1>No Results</h1>
+                    <p>
+                        Your search brought up no results. Try using a different keyword. Or try typying all
+                        to see all items in the demo. These can be linked to anything you want.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+		<div class="d-flex px-3 mb-n3">
+            <div class="align-self-center">
+                <h4 class="mb-0">Recommended</h4>
+            </div>
+            <div class="align-self-center ms-auto">
+                <a href="#" class="font-12">View All</a>
+            </div>
+        </div>
+
+        <div class="splide double-slider slider-no-dots visible-slider" id="double-slider-1a">
+            <div class="splide__track">
+                <div class="splide__list">
+                    <div class="splide__slide">
+                        <a href="#" class="mx-3" data-menu="menu-reserve">
+                            <div class="card card-style me-0 mb-0" style="background-image:url(images/travel/4m.jpg);" data-card-height="250">
+								<div class="card-top p-2">
+									<span class="color-black bg-white px-2 py-1 rounded-xs font-11"><i class="fa fa-star color-yellow-dark pe-2"></i>4.95</span>
+								</div>
+                                <div class="card-bottom p-2 px-2">
+                                    <h4 class="color-white line-height-s">Maldive<br> Islands</h4>
+									<span class="color-white font-10 opacity-60"><i class="fa fa-map-marker pe-2"></i>Indian Ocean</span>
+                                </div>
+                                <div class="card-overlay bg-gradient"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="splide__slide">
+                        <a href="#" class="mx-3" data-menu="menu-reserve">
+                            <div class="card card-style me-0 mb-0" style="background-image:url(images/travel/2m.jpg);" data-card-height="250">
+                                <div class="card-top p-2">
+									<span class="color-black bg-white px-2 py-1 rounded-xs font-11"><i class="fa fa-star color-yellow-dark pe-2"></i>4.92</span>
+								</div>
+								<div class="card-bottom p-2 px-2">
+									<h4 class="color-white line-height-s">Mount Rushmore</h4>
+									<span class="color-white font-10 opacity-60"><i class="fa fa-map-marker pe-2"></i>United States</span>
+								</div>
+                                <div class="card-overlay bg-gradient"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="splide__slide">
+                        <a href="#" class="mx-3" data-menu="menu-reserve">
+                            <div class="card card-style me-0 mb-0" style="background-image:url(images/travel/7m.jpg);" data-card-height="250">
+								<div class="card-top p-2">
+									<span class="color-black bg-white px-2 py-1 rounded-xs font-11"><i class="fa fa-star color-yellow-dark pe-2"></i>4.87</span>
+								</div>
+								<div class="card-bottom p-2 px-2">
+									<h4 class="color-white line-height-s">Blue<br> Lagoon</h4>
+									<span class="color-white font-10 opacity-60"><i class="fa fa-map-marker pe-2"></i>Europe, Iceland</span>
+								</div>
+								<div class="card-overlay bg-gradient"></div>
+							</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex px-3 mb-2">
+            <div class="align-self-center">
+                <h4 class="mb-0">Last Minute Deal</h4>
+            </div>
+            <div class="align-self-center ms-auto">
+                <a href="#" class="font-12">View All</a>
+            </div>
+        </div>
+
+        <div class="card card-style" style="background-image:url(images/travel/6m.jpg)" data-card-height="260">
+            <div class="card-top p-3">
+                <a href="#" data-menu="menu-reserve" class="btn btn-s bg-white color-black rounded-s scale-box font-700 text-uppercase float-end">Get Offer</a>
+            </div>
+            <div class="card-bottom m-2">
+                <div class="d-block px-2 py-2 rounded-m">
+					<div class="d-flex">
+						<div class="pe-3">
+							<h1 class="color-white font-23 font-800">Maldive Islands</h1>
+							<p class="color-white font-12 mb-0 line-height-s opacity-70">Enjoy an amazing week on the most beautiful islands on earth.</p>
+						</div>
+						<div class="w-50 align-self-center text-end ms-auto">
+							<h1 class="color-white">$1.450</h1>
+							<p class="color-white mb-0 mt-n2 font-9 line-height-xs">
+								All Expenses Paid
+							</p>
+						</div>
+					</div>
+                </div>
+            </div>
+            <div class="card-overlay bg-gradient"></div>
+        </div>
+
+
+		<div class="d-flex px-3">
+			<div class="align-self-center">
+				<h4 class="mb-0">Great Deals</h4>
 			</div>
-		</div> -->
+			<div class="align-self-center ms-auto">
+				<a href="#" class="font-12">View All</a>
+			</div>
+		</div>
+
 		<div class="content mt-2 mb-n3">
 			<div class="row">
-			<?php
-			include('./config.php');
-					$sqlActividades1 = ("SELECT * FROM `actividades`");
-					$queryActividades1 = mysqli_query($con, $sqlActividades1);
-					?>
-					<?php 
-                    while ($dataActividad1 = mysqli_fetch_array($queryActividades1)) { ?>
 				<div class="col-6 pe-2">
-					<a href="actividad.php?id=<?php echo $dataActividad1['id']; ?>" data-menu="menu-reserve" class="card card-style mx-0 mb-3">
-						<!-- <div class="card-top m-2">
-							<span class="bg-theme color-black font-11 px-2 py-1 font-700 rounded-xs shadow-xxl">15% OFF</span>
-						</div> -->
-						<img src="images/media/<?php echo $dataActividad1['id']; ?>_img1.jpg" alt="img" class="img-fluid">
-						<div class="p-2">
-							<h4 class="mb-0"><?php echo ucfirst($dataActividad1['actividad']); ?></h4>
-							<div class="divider mt-1 mb-1"></div>
-							<h5 class="font-17 mb-0">S/. <?php echo $dataActividad1['precio']; ?><span class="float-end font-400 font-11"><i class="fa fa-clock pe-1"></i><?php echo $dataActividad1['tiempo']; ?></span></h5>
+					<a href="#" data-menu="menu-reserve" class="card card-style mx-0 mb-3">
+						<div class="card-top m-2">
+							<span class="bg-white color-black font-11 px-2 py-1 font-700 rounded-xs shadow-xxl">15% OFF</span>
 						</div>
-						
+						<img src="images/travel/4m.jpg" alt="img" class="img-fluid">
+						<div class="p-2">
+							<h4 class="mb-0">Island Exploring</h4>
+							<p class="mb-0 font-11 mt-n1 opacity-70"><i class="fa fa-map-marker pe-2"></i>Europe, Iceland</p>
+						</div>
+						<div class="divider mb-0"></div>
+						<h5 class="p-2 font-12">$1550 / 7 Days <span class="float-end font-400 font-11 color-green-dark">30 Left</span></h5>
 					</a>
 				</div>
-				<?php } 
-                    ?>
-				
+				<div class="col-6 ps-2">
+					<a href="#" data-menu="menu-reserve" class="card card-style mx-0 mb-3">
+						<div class="card-top m-2">
+							<span class="bg-white color-black font-11 px-2 py-1 font-700 rounded-xs shadow-xxl">25% OFF</span>
+						</div>
+						<img src="images/travel/7m.jpg" alt="img" width="100" class="img-fluid">
+						<div class="p-2">
+							<h4 class="mb-0">Mountain Hikes</h4>
+							<p class="mb-0 font-11 mt-n1 opacity-70"><i class="fa fa-map-marker pe-2"></i>Europe, France</p>
+						</div>
+						<div class="divider mb-0"></div>
+						<h5 class="p-2 font-12">$1320 / 5 Days <span class="float-end font-400 font-11 color-yellow-dark">5 Left</span></h5>
+					</a>
+				</div>
+				<div class="col-6 pe-2">
+					<a href="#" data-menu="menu-reserve" class="card card-style mx-0 mb-3">
+						<div class="card-top m-2">
+							<span class="bg-white color-black font-11 px-2 py-1 font-700 rounded-xs shadow-xxl">15% OFF</span>
+						</div>
+						<img src="images/travel/3m.jpg" alt="img" class="img-fluid">
+						<div class="p-2">
+							<h4 class="mb-0">Forest Walks</h4>
+							<p class="mb-0 font-11 mt-n1 opacity-70"><i class="fa fa-map-marker pe-2"></i>United States</p>
+						</div>
+						<div class="divider mb-0"></div>
+						<h5 class="p-2 font-12">$150 / 1 Day <span class="float-end font-400 font-11 color-red-dark">0 Left</span></h5>
+					</a>
+				</div>
+				<div class="col-6 ps-2">
+					<a href="#" data-menu="menu-reserve" class="card card-style mx-0 mb-3">
+						<div class="card-top m-2">
+							<span class="bg-white color-black font-11 px-2 py-1 font-700 rounded-xs shadow-xxl">25% OFF</span>
+						</div>
+						<img src="images/travel/5m.jpg" alt="img" width="100" class="img-fluid">
+						<div class="p-2">
+							<h4 class="mb-0">Ocean Dives</h4>
+							<p class="mb-0 font-11 mt-n1 opacity-70"><i class="fa fa-map-marker pe-2"></i>Europe, France</p>
+						</div>
+						<div class="divider mb-0"></div>
+						<h5 class="p-2 font-12">$530 / 2 Dives <span class="float-end font-400 font-11 color-blue-dark">10 Left</span></h5>
+					</a>
+				</div>
 			</div>
 		</div>
 
---
+++ menu1.php
@@ -0,0 +1,133 @@
+<!DOCTYPE HTML>
+<?php include_once './assets/head.php'?>
+<title>LunApp - Menú</title>
+</head>
+
+
+<body class="theme-light" data-highlight="highlight-red" data-gradient="body-default">
+
+<div id="preloader"><div class="spinner-border color-highlight" role="status"></div></div>
+
+<div id="page">
+
+	<?php include_once './assets/header-footer-menu.php'?>
+
+    <div class="page-content">
+
+		<div class="card card-style mx-0 mb-0 rounded-0" data-card-height="400" style="background-image:url('images/pictures_vertical/bg1.jpg')">
+			<div class="card-top">
+				
+			</div>
+			<div class="card-bottom mb-4">
+				<h1 class="color-white text-center mb-0 font-28">Atractivos de Lunahuana</h1>
+				<p class="color-white text-center mb-3 ">Encuentra los sitios más visitados aquí!</p>
+			</div>
+			<div class="card-overlay bg-gradient rounded-0"></div>
+		</div>
+
+        <div class="content mt-n4">
+            <div class="search-box bg-theme color-theme rounded-m shadow-l">
+                <i class="fa fa-search"></i>
+                <input type="text" class="border-0" placeholder="Buscar una actividad" data-search id="search-input">
+				<a href="#" class="clear-search disabled no-click mt-0"></a>
+            </div>
+            <div class="search-results disabled-search-list mt-3">
+                <div class="card card-style mx-0 px-2 py-2 mb-0">
+					<a href="#" class="d-flex py-1" data-filter-name="all">
+                        <div class="ps-2 align-self-center">
+							<h6 class="font-12 opacity-60 mb-2">ACTIVIDAD</h6>
+						</div>
+						<div class="pe-2 align-self-center ms-auto">
+							<h6 class="font-12 opacity-60 mb-2 text-end">PRECIO</h6>
+							<h6 class="font-12 opacity-60 mb-2 text-end">DURACIÓN</h6>
+						</div>
+					<a>
+					<a data-filter-name="all" class="divider mb-1 mx-2"></a>
+					<?php
+					include('./config.php');
+					$sqlActividades = ("SELECT * FROM `actividades`");
+					$queryActividades = mysqli_query($con, $sqlActividades);
+					?>
+					<?php 
+                    while ($dataActividad = mysqli_fetch_array($queryActividades)) { ?>
+                    <a href="actividad.php?id=<?php echo $dataActividad['id']; ?>" class="d-flex" data-filter-item data-filter-name="all <?php echo strtolower($dataActividad['actividad']); ?>">
+                        <div class="ps-2 align-self-center">
+                            <strong class="color-theme font-16 d-block"><?php echo strtolower($dataActividad['actividad']); ?></strong>
+                        </div>
+                        <div class="ms-auto text-end align-self-center pe-2">
+							<h5 class="line-height-xs font-14 pt-1 mb-1 opacity-40">S/. <?php echo $dataActividad['precio']; ?></h5>
+                            <h5 class="line-height-xs font-14 opacity-30"><?php echo trim($dataActividad['tiempo'],"."); ?></h5>
+						</div>
+                    </a>
+					<?php } 
+                    ?>
+                    
+                </div>
+            </div>	
+        </div>
+
+		<!-- <div class="content mt-2 mb-n3">
+			<h5 class="mb-2">Lo más visto</h5>
+			<div class="row">
+				<div class="col-6 ps-2">
+					
+					<a href="" class="card card-style mx-0 mb-3">
+						<div class="card-top m-2">
+							<span class="bg-highlight color-black font-11 px-2 py-1 font-700 rounded-xs shadow-xxl">15% OFF</span>
+						</div>
+						<img src="images/media/img4.jpg" alt="img" class="img-fluid">
+						<div class="p-2 pb-0">
+							<h4 class="mb-1"></h4>
+							<p class="pb-1 mb-0 font-11 mt-n1 opacity-70"></i>a</p>
+						</div>
+						
+
+					</a>
+					
+				</div>
+			</div>
+		</div> -->
+		<div class="content mt-2 mb-n3">
+			<div class="row">
+			<?php
+			include('./config.php');
+					$sqlActividades1 = ("SELECT * FROM `actividades`");
+					$queryActividades1 = mysqli_query($con, $sqlActividades1);
+					?>
+					<?php 
+                    while ($dataActividad1 = mysqli_fetch_array($queryActividades1)) { ?>
+				<div class="col-6 pe-2">
+					<a href="actividad.php?id=<?php echo $dataActividad1['id']; ?>" data-menu="menu-reserve" class="card card-style mx-0 mb-3">
+						<!-- <div class="card-top m-2">
+							<span class="bg-theme color-black font-11 px-2 py-1 font-700 rounded-xs shadow-xxl">15% OFF</span>
+						</div> -->
+						<img src="images/media/<?php echo $dataActividad1['id']; ?>_img1.jpg" alt="img" class="img-fluid">
+						<div class="p-2">
+							<h4 class="mb-0"><?php echo ucfirst($dataActividad1['actividad']); ?></h4>
+							<div class="divider mt-1 mb-1"></div>
+							<h5 class="font-17 mb-0">S/. <?php echo $dataActividad1['precio']; ?><span class="float-end font-400 font-11"><i class="fa fa-clock pe-1"></i><?php echo $dataActividad1['tiempo']; ?></span></h5>
+						</div>
+						
+					</a>
+				</div>
+				<?php } 
+                    ?>
+				
+			</div>
+		</div>
+
+        <?php include_once './assets/footer.php'?>
+
+    </div>
+    <!-- End of Page Content-->
+	
+    <!-- All Menus, Action Sheets, Modals, Notifications, Toasts, Snackbars get Placed outside the <div class="page-content"> -->
+    <?php include_once './assets/menu-footer.php'?>
+
+</div>
+
+<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="scripts/custom.js"></script>
+
+
+</body>
