b569933fd84863b78a7cb48fc86d3a6840b91d44
Carlos <1971975680@undc.edu.pe>
2023-10-18 16:49:02 -0500

    wa

--- qr.php
+++ qr.php
@@ -30,9 +30,10 @@ while ($dataCIISIC = mysqli_fetch_array($queryCIISIC)) { ?>
                         <span class="color-highlight font-300 d-block text-uppercase font-10 pt-3"><?php echo $dataCIISIC['id']; ?></span>
                         <div class="d-flex pb-0">
                             <div class="align-self-center flex-grow-1">
-                                <strong class="color-theme font-20 d-block mt-0 mb-0 pb-0"><?php echo $dataCIISIC['nombres']; ?> <?php echo $dataCIISIC['apellidos']; ?></strong>
+                                <strong class="color-theme font-20 d-block mt-0 mb-0 pb-0"><i
+                                        class="fa fa-user pe-1"></i><?php echo $dataCIISIC['nombres']; ?> <?php echo $dataCIISIC['apellidos']; ?></strong>
                                 <span class="font-11 color-theme opacity-30 d-block pb-2"><i
-                                        class="fa fa-clock pe-1"></i><?php echo $dataCIISIC['dni']; ?></span>
+                                        class="fa fa-user pe-1"></i><?php echo $dataCIISIC['dni']; ?></span>
 
                             </div>
                         </div>
