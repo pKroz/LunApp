b5ef37adbb206c30294355b314cb517d5cdff613
Carlos <1971975680@undc.edu.pe>
2023-10-18 18:31:42 -0500

    wa

--- qr.php
+++ qr.php
@@ -61,31 +61,31 @@ while ($dataCIISIC = mysqli_fetch_array($queryCIISIC)) { ?>
                     <div class="col-12">
                             <div class="d-flex">
                                 <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
-                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">DNI: </strong><strong class="align-self-center ps-1 font-13 color-white"><?php echo $dataCIISIC['dni']; ?></strong>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">DNI: </strong><strong class="align-self-center ps-1 font-13 color-theme"><?php echo $dataCIISIC['dni']; ?></strong>
                             </div>
                         </div>
                         <div class="col-12">
                             <div class="d-flex">
                                 <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
-                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">MODALIDAD: </strong><strong class="align-self-center ps-1 font-13 color-white"><?php echo $dataCIISIC['modalidad']; ?></strong>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">MODALIDAD: </strong><strong class="align-self-center ps-1 font-13 color-theme"><?php echo $dataCIISIC['modalidad']; ?></strong>
                             </div>
                         </div>
                         <div class="col-12">
                             <div class="d-flex">
                                 <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
-                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">TIPO DE INSCRIPCIÓN: </strong><strong class="align-self-center ps-1 font-13 color-white"><?php echo $dataCIISIC['tipo']; ?></strong>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">TIPO DE INSCRIPCIÓN: </strong><strong class="align-self-center ps-1 font-13 color-theme"><?php echo $dataCIISIC['tipo']; ?></strong>
                             </div>
                         </div>
                         <div class="col-12">
                             <div class="d-flex">
                                 <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
-                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">CORREO: </strong><strong class="align-self-center ps-1 font-13 color-white"><?php echo $dataCIISIC['correo']; ?></strong>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">CORREO: </strong><strong class="align-self-center ps-1 font-13 color-theme"><?php echo $dataCIISIC['correo']; ?></strong>
                             </div>
                         </div>
                         <div class="col-12">
                             <div class="d-flex">
                                 <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
-                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">CELULAR: </strong><strong class="align-self-center ps-1 font-13 color-white"><a href="https://wa.me/51<?php echo $dataCIISIC['celular']; ?>" class="color-white"><?php echo $dataCIISIC['celular']; ?></a></strong>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">CELULAR: </strong><strong class="align-self-center ps-1 font-13 color-theme"><a href="https://wa.me/51<?php echo $dataCIISIC['celular']; ?>" class="color-white"><?php echo $dataCIISIC['celular']; ?></a></strong>
                             </div>
                         </div>
                     </div>
