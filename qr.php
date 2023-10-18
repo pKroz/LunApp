0ecea5f36f3d92260c071f356aaa3e08f42b0480
Carlos <1971975680@undc.edu.pe>
2023-10-18 17:31:42 -0500

    WA

--- qr.php
+++ qr.php
@@ -6,7 +6,8 @@
 				$sqlCIISIC = ("SELECT * FROM `ciisic` WHERE dni=$dni or id=$dni");
 				$queryCIISIC = mysqli_query($con, $sqlCIISIC);
 			?>
-<?php 
+<?php
+if (mysqli_num_rows($queryCIISIC) > 0) {
 while ($dataCIISIC = mysqli_fetch_array($queryCIISIC)) { ?>
 <title>QR CIISIC - <?php echo $dataCIISIC['apellidos']; ?></title>
 </head>
@@ -79,8 +80,7 @@ while ($dataCIISIC = mysqli_fetch_array($queryCIISIC)) { ?>
                         class="btn btn-full btn-m rounded-sm shadow-xl bg-highlight font-700 text-uppercase">VER COMPROBANTE DE PAGO</a>
                     </div>
                 </div>
-                <?php } 
-            ?>
+                <?php } ?>
             </div>
 
 
@@ -96,3 +96,83 @@ while ($dataCIISIC = mysqli_fetch_array($queryCIISIC)) { ?>
     <script type="text/javascript" src="scripts/bootstrap.min.js"></script>
     <script type="text/javascript" src="scripts/custom.js"></script>
 </body>
+<?php
+} else {?>
+    <title>QR CIISIC - NO INSCRITO</title>
+</head>
+
+<body class="theme-light" data-highlight="highlight-red" data-gradient="body-default">
+
+    <div id="preloader">
+        <div class="spinner-border color-highlight" role="status"></div>
+    </div>
+
+    <div id="page">
+
+        <?php include_once './assets/header-footer-qr.php'?>
+
+        <div class="page-content header-clear-medium">
+
+            <div class="card card-style">
+
+                <div class="content mt-n5 pt-3">
+                    <div>
+                        <span class="color-highlight font-300 d-block text-uppercase font-10 pt-3">SIN ID</span>
+                        <div class="d-flex pb-0">
+                            <div class="align-self-center flex-grow-1">
+                                <strong class="color-theme font-20 d-block mt-0 mt-3 mb-3 pb-0"><i
+                                        class="fa fa-user pe-2"></i>QR INVALIDO - NO INSCRITO</strong>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="divider mb-2"></div>
+                    <div class="row mb-2">
+                    <div class="col-12">
+                            <div class="d-flex">
+                                <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">DNI: </strong><strong class="align-self-center ps-1 font-13 color-white">-</strong>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-flex">
+                                <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">MODALIDAD: </strong><strong class="align-self-center ps-1 font-13 color-white">-</strong>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-flex">
+                                <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">TIPO DE INSCRIPCIÓN: </strong><strong class="align-self-center ps-1 font-13 color-white">-</strong>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-flex">
+                                <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">CORREO: </strong><strong class="align-self-center ps-1 font-13 color-white">-</strong>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-flex">
+                                <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
+                                <strong class="font-13 ps-1 color-theme opacity-50 d-block">CELULAR: </strong><strong class="align-self-center ps-1 font-13 color-white">-</strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <?php include_once './assets/footerqr.php'?>
+
+        </div>
+
+        <?php include_once './assets/menu-footer.php'?>
+
+    </div>
+
+    <script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+    <script type="text/javascript" src="scripts/custom.js"></script>
+</body>
+<?php } ?>
