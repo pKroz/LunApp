5a51ab51c973d8c695d91782804e5a7daddc51b8
Carlos <1971975680@undc.edu.pe>
2023-10-18 17:09:08 -0500

    wa

--- assets/footerqr.php
+++ assets/footerqr.php
@@ -2,7 +2,6 @@
             <a href="#" class="footer-title"><span class="color-highlight">CIISIC QR</span></a>
             <p class="footer-text"><span>Aplicativo para verificar inscripción al V CIISIC</p>
             <div class="text-center mb-3">
-                <a href="#" class="icon icon-xs rounded-sm shadow-l me-1 bg-facebook"><i class="fab fa-facebook-f"></i></a>
                 <a href="#" data-menu="menu-share" class="icon icon-xs rounded-sm me-1 shadow-l bg-highlight"><i class="fa fa-share-alt "></i></a>
                 <a href="#" class="back-to-top icon icon-xs rounded-sm shadow-l bg-dark-light"><i class="fa fa-angle-up"></i></a>
             </div>
