0e36aa53d56b8d9f8cb17d0a56a1c21b95c3203d
Carlos <1971975680@undc.edu.pe>
2023-10-18 16:43:58 -0500

    wa

--- qr.php
+++ qr.php
@@ -36,27 +36,9 @@ while ($dataCIISIC = mysqli_fetch_array($queryCIISIC)) { ?>
                                         class="fa fa-clock pe-1"></i><?php echo $dataCIISIC['id']; ?></span>
 
                             </div>
-                            <div class="align-self-center flex-shrink-1">
-                                <a href="#"
-                                    class="btn btn-full btn-sm font-900 text-uppercase rounded-sm shadow-xxl bg-highlight mb-3 d-flex justify-content-center"><i
-                                        class="fa fa-vr-cardboard pe-2 me-1 font-15 color-white"></i>Tour</a>
-                            </div>
                         </div>
                     </div>
-                    <div class="card card-style">
-<a href="#" data-menu="portfolio-1" class="card card-style mx-0 mb-0 rounded-0 preload-img entered loaded" data-src="images/pictures/21w.jpg" data-card-height="200" style="height: 200px; background-image: url(&quot;images/pictures/21w.jpg&quot;);" data-ll-status="loaded">
-<div class="card-bottom ms-3 mb-3">
-<h1 class="color-white mb-0">Portfolio Title 1</h1>
-</div>
-<div class="card-overlay bg-gradient rounded-0"></div>
-</a>
-<div class="content">
-<p>A small description about your project goes here. Then, you can access the portfolio project to see it in detail.</p>
-<a href="#" class="color-theme font-12"><i class="fa fa-link font-10 me-2"></i>View More: <span class="color-highlight">Project Title 1</span></a><br>
-<a href="#" class="color-theme font-12"><i class="fa fa-bookmark font-10 me-2"></i>Built For: <span class="color-highlight">Envato Marketplaces</span></a><br>
-<a href="#" class="color-theme font-12"><i class="fa fa-tag font-10 me-2"></i>Tags: <span class="color-highlight">Portfolio, Product, Images</span></a><br>
-</div>
-</div>
+
 
 
                     <div class="divider mb-4"></div>
@@ -65,7 +47,7 @@ while ($dataCIISIC = mysqli_fetch_array($queryCIISIC)) { ?>
                     <p class="mb-4">
                     <?php echo $dataCIISIC['id']; ?>
                     </p>
-                    <div class="card rounded-sm overflow-hidden" data-card-height="200">
+                    <div class="card rounded-sm overflow-hidden" data-card-height="650">
                         <div class="bg-black opacity-60"></div>
                         <img src="https://drive.google.com/uc?export=view&id=<?php echo $dataCIISIC['idembed']; ?>">
                     </div>
@@ -73,8 +55,7 @@ while ($dataCIISIC = mysqli_fetch_array($queryCIISIC)) { ?>
                     <div class="divider mb-4"></div>
                     <h3>Detalles</h3>
                     <p class="mb-2">
-                        Te mostramos algunos datos que debes tomar en cuenta para realizar satisfactoriamente esta
-                        actividad.
+                        Si
                     </p>
                     <div class="divider mt-4"></div>
                     <a href="<?php echo $dataCIISIC['comprobante']; ?>" data-menu="menu-reserve"
