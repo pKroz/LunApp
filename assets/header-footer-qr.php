3449c9abd6984f89134ae21609913ffc7f0ec3c7
Carlos <1971975680@undc.edu.pe>
2023-10-18 19:11:14 -0500

    wa

--- assets/header-footer-qr.php
+++ assets/header-footer-qr.php
@@ -1,5 +1,5 @@
 <div class="header header-fixed header-logo-center">
-        <a href="menu.php" class="header-title back-to-top">Verificar Inscripción</a>
+        <a href="" class="header-title back-to-top">CIISIC - Inscripciones</a>
         <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-4"><i class="fas fa-lightbulb"></i></a>
     </div>
