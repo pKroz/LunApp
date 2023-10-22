3588cd8c6e42b974ffd970e39190c09b0e9a8698
Carlos <1971975680@undc.edu.pe>
2023-10-22 16:11:10 -0500

    wa

--- actividad.php
+++ actividad.php
@@ -25,31 +25,18 @@ while ($dataActividad = mysqli_fetch_array($queryActividades)) { ?>
 
             <div class="card card-style">
                 <div class="splide single-slider slider-no-dots" id="single-slider-home">
-                        
+                    <div class="splide__track">
+                        <div class="splide__list">
+
                             <div class="splide__slide">
                                 <div class="card rounded-0 shadow-l">
                                     <img class="img-fluid"
                                         src="images/media/<?php echo $dataActividad['id']; ?>_img1.jpg">
                                 </div>
                             </div>
-                            <div class="splide__slide">
-                                <div class="card rounded-0 shadow-l">
-                                    <img class="img-fluid"
-                                        src="images/media/<?php echo $dataActividad['id']; ?>_img2.jpg">
-                                </div>
-                            </div>
-                            <div class="splide__slide">
-                                <div class="card rounded-0 shadow-l">
-                                    <img class="img-fluid"
-                                        src="images/media/<?php echo $dataActividad['id']; ?>_img3.jpg">
-                                </div>
-                            </div>
-                            <div class="splide__slide">
-                                <div class="card rounded-0 shadow-l">
-                                    <img class="img-fluid"
-                                        src="images/media/<?php echo $dataActividad['id']; ?>_img4.jpg">
-                                </div>
-                            </div>
+                           
+                        </div>
+                    </div>
                 </div>
 
 
