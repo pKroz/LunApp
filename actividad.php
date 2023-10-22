f330252999c1a824a37b530fe21e9740e4e7feb4
Carlos <1971975680@undc.edu.pe>
2023-10-22 16:25:32 -0500

    wa

--- actividad.php
+++ actividad.php
@@ -34,7 +34,24 @@ while ($dataActividad = mysqli_fetch_array($queryActividades)) { ?>
                                         src="images/media/<?php echo $dataActividad['id']; ?>_img1.jpg">
                                 </div>
                             </div>
-                           
+                            <div class="splide__slide">
+                                <div class="card rounded-0 shadow-l">
+                                    <img class="img-fluid"
+                                        src="images/media/<?php echo $dataActividad['id']; ?>_img2.jpg">
+                                </div>
+                            </div>
+                            <div class="splide__slide">
+                                <div class="card rounded-0 shadow-l">
+                                    <img class="img-fluid"
+                                        src="images/media/<?php echo $dataActividad['id']; ?>_img3.jpg">
+                                </div>
+                            </div>
+                            <div class="splide__slide">
+                                <div class="card rounded-0 shadow-l">
+                                    <img class="img-fluid"
+                                        src="images/media/<?php echo $dataActividad['id']; ?>_img4.jpg">
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -42,13 +59,11 @@ while ($dataActividad = mysqli_fetch_array($queryActividades)) { ?>
 
                 <div class="content mt-n5 pt-3">
                     <div>
-                        <span class="color-highlight font-300 d-block text-uppercase font-10 pt-3">Actividades en
-                            Lunahuana</span>
+                        <span class="color-highlight font-300 d-block text-uppercase font-10 pt-3">Actividades en Lunahuana</span>
 
                         <div class="d-flex pb-0">
                             <div class="align-self-center flex-grow-1">
-                                <strong
-                                    class="color-theme font-20 d-block mt-0 mb-0 pb-0"><?php echo $dataActividad['actividad']; ?></strong>
+                                <strong class="color-theme font-20 d-block mt-0 mb-0 pb-0"><?php echo $dataActividad['actividad']; ?></strong>
                                 <span class="font-11 color-theme opacity-30 d-block pb-2"><i
                                         class="fa fa-clock pe-1"></i><?php echo $dataActividad['tiempo']; ?></span>
 
@@ -67,10 +82,14 @@ while ($dataActividad = mysqli_fetch_array($queryActividades)) { ?>
 
                     <h3>¿En qué consiste la actividad?</h3>
                     <p class="mb-4">
-                        <?php echo $dataActividad['descripcion']; ?>
+                    <?php echo $dataActividad['descripcion']; ?>
                     </p>
-
-
+                    <!-- <div class="card rounded-sm overflow-hidden splide single-slider slider-no-dots" data-card-height="400">
+                        <div class="bg-black opacity-60"></div>
+                        <iframe
+                            src="embed"
+                            style="height:400px;" frameborder='0' allowfullscreen></iframe>
+                    </div> -->
                     <div class="content">
                         <div class="row mb-0">
 
@@ -134,11 +153,10 @@ while ($dataActividad = mysqli_fetch_array($queryActividades)) { ?>
                                     class="align-self-center ps-1 font-13 color-theme"><?php echo $dataActividad['r4']; ?></strong>
                             </div>
                         </div>
-
+                   
                     </div>
                     <div class="divider mt-4"></div>
-                    <a href="https://api.whatsapp.com/send/?phone=51<?php echo $dataActividad['reserva']; ?>&text=Quisiera reservar la actividad de <?php echo ucfirst($dataActividad['actividad']); ?>"
-                        data-menu="menu-reserve"
+                    <a href="https://api.whatsapp.com/send/?phone=51<?php echo $dataActividad['reserva']; ?>&text=Quisiera reservar la actividad de <?php echo ucfirst($dataActividad['actividad']); ?>" data-menu="menu-reserve"
                         class="btn btn-full btn-m rounded-sm shadow-xl bg-highlight font-700 text-uppercase ">Realizar
                         reserva</a>
 
