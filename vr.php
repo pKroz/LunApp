e1cbff3caa4be591731a632f71b90faaecdb308d
Carlos <1971975680@undc.edu.pe>
2023-11-23 18:56:21 -0500

    wa

--- vr.php
+++ vr.php
@@ -1,6 +1,20 @@
 <!DOCTYPE HTML>
-<?php include_once './assets/head.php'?>
-<title>QR CIISIC</title>
+<html lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+<link rel="stylesheet" type="text/css" href="styles/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="styles/style.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i|Source+Sans+Pro:300,300i,400,400i,600,600i,700,700i,900,900i&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="fonts/css/fontawesome-all.min.css">
+<script type="text/javascript" src="scripts/xd.js"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+<link rel="manifest" href="_manifest.json" data-pwa-version="set_in_manifest_and_pwa_js">
+<link rel="apple-touch-icon" sizes="180x180" href="app/icons/icon-192x192.png">
+<link rel="shortcut icon" href="app/icons/favicon.ico" />
+<title>VR - LunApp</title>
 </head>
 <body class="theme-light" data-highlight="highlight-red" data-gradient="body-default">
     <div id="preloader">
@@ -15,14 +29,13 @@
                         <div class="row mb-2">
                             <div class="col-12">
                                     <div class="d-flex">
-                                        <i class="mt-1 fa fa-plus color-highlight font-14 fa-fw text-center"></i>
                                         <iframe name="canetelab" src="https://framevr.io/canetelab" height="100%" width="100%" allow="camera;microphone;display-capture;xr-spatial-tracking" scrolling="no" frameBorder="0"></iframe>
                                     </div>
                             </div>
                         </div>
                 </div>
             </div>
-            <?php include_once './assets/footerqr.php'?>
+            <?php include_once './assets/footer.php'?>
         </div>
         <?php include_once './assets/menu-footer.php'?>
     </div>
