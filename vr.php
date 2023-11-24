e4767144534e92fc9b924538ffa9a733448efc7f
Carlos <1971975680@undc.edu.pe>
2023-11-23 19:00:17 -0500

    wa

--- vr.php
+++ vr.php
@@ -26,17 +26,17 @@
         <div class="spinner-border color-highlight" role="status"></div>
     </div>
     <div id="page">
-        <div class="header header-fixed header-logo-center header-auto-show">
-            <a href="menu.php" class="header-title back-to-top">Travel & Tour</a>
-            <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
-            <a href="#" data-toggle-theme class="header-icon header-icon-4"><i class="fas fa-lightbulb"></i></a>
-        </div>
+    <div class="header header-fixed header-logo-center">
+        <a href="menu.php" class="header-title back-to-top">Travel & Tour</a>
+        <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
+        <a href="#" data-toggle-theme class="header-icon header-icon-4"><i class="fas fa-lightbulb"></i></a>
+    </div>
 
-        <div id="footer-bar" class="footer-bar-1">
-            <a href="info.php"><i class="fa fa-info"></i><span>Información</span></a>
-            <a href="menu.php" class="active-nav"><i class="fa fa-home"></i><span>Inicio</span></a>
-            <a href="#" data-menu="menu-settings"><i class="fa fa-cog"></i><span>Ajustes</span></a>
-        </div>
+    <div id="footer-bar" class="footer-bar-1"> 
+        <a href="info.php"><i class="fa fa-info"></i><span>Información</span></a>
+        <a href="menu.php"><i class="fa fa-home"></i><span>Inicio</span></a>
+        <a href="#" data-menu="menu-settings"><i class="fa fa-cog"></i><span>Ajustes</span></a>
+    </div>
         <div class="page-content header-clear-medium">
             <div class="card card-style">
                 <div class="content mt-n5 pt-3">
