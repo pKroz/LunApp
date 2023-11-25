fb9fd799127ec2dbdd929d5e4b2391e1b16f2341
Carlos <1971975680@undc.edu.pe>
2023-11-25 01:03:37 -0500

    wa

--- v1.php
+++ v1.php
@@ -293,4 +293,5 @@
                 </div>
             </div>
             <script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+            <script type="text/javascript" src="scripts/custom.js"></script>
 </body>
